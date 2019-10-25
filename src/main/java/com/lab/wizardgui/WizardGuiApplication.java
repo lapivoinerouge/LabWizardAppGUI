7d0b53b7f2b799f2224eb48f602caaf464fb85a2
lapivoinerouge <wioladys@gmail.com>
2019-10-25 16:03:41 +0200

    init

---
+++ src/main/java/com/lab/wizardgui/WizardGuiApplication.java
@@ -0,0 +1,13 @@
+package com.lab.wizardgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizardGuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab/wizardgui/WizardGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.wizardgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
